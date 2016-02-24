528f7e39e88f39ea40fb95f0b09608285e7ad85b
johnny9052 <alexander9052@gmail.com>
2016-02-24 14:29:22 -0500

    primer commit

---
+++ Proyecto/Index.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Proyecto.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <%--URL WEBSERVICE http://wsf.cdyne.com/WeatherWS/Weather.asmx--%>
+        <asp:TextBox ID="txtCodigo" runat="server" ToolTip="Codigo a buscar" Text="22041"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Llamar web service" OnClick="LlamarWebService"/>
+        <asp:Label ID="lblRespuesta" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Proyecto/Index.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proyecto
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void LlamarWebService(object sender, EventArgs e)
+        {
+            ServiceReferenceWeather.WeatherSoapClient cliente = new
+                ServiceReferenceWeather.WeatherSoapClient("WeatherSoap");//Nombre del WS en el config
+
+            ServiceReferenceWeather.WeatherReturn resultado = cliente.GetCityWeatherByZIP(txtCodigo.Text);
+
+            lblRespuesta.Text = resultado.City + " - " + resultado.Description + " - " +
+                                resultado.RelativeHumidity;
+        }
+    }
+}
